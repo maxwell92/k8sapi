b75fdf687af2a29683a421f5904ce33c6d045176
maxwell92 <710028463@qq.com>
2016-07-10 16:35:16 +0800

    basic web server written in one golang file

---
+++ basicwebserver.go
@@ -0,0 +1,50 @@
+package main
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"encoding/json"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+
+type Todo struct {
+	Name 	string 	`json:"name"`
+	Completed bool 	`json:"completed"`
+	Due 	time.Time 	`json:"due"`
+}
+
+type Todos []Todo
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, ", html.EscapeString(r.URL.Path))
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+
+	json.NewEncoder(w).Encode(todos)
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Todo Show: ", todoId)
+}
+
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoId}", TodoShow)
+
+	log.Fatal(http.ListenAndServe(":10000", router))
+	
+}
