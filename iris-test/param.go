9c957a4b3946ad0352e15dd3bd6a4845b369a477
maxwell92 <710028463@qq.com>
2016-08-12 19:41:47 +0800

    API test

--- iris-test/param.go
+++ iris-test/param.go
@@ -15,8 +15,23 @@ func (ac AbcController) Get() {
 	ac.Write("Get from /abc/" + id)
 }
 
+/*
+func (ac AbcController) Get() {
+	fmt.Println("get from Get")
+	ac.Write("get from /abc")
+}
+*/
+func (ac AbcController) Post() {
+	oid := ac.Param("oid")
+	lid := ac.Param("lid")
+	fmt.Printf("post from /abc/%s/lalala/%s\n", oid, lid)
+	ac.Write("post from /abc/" + oid + "/lalala/" + lid)
+}
+
 func main() {
 	abc := new(AbcController)
-	iris.API("/abc/:id", *abc)
+	//iris.API("/abc/:id", *abc)
+	iris.API("/abc", *abc)
+	iris.API("/abc/:oid/lalala/:lid", *abc)
 	iris.Listen(":10000")
 }
