5e8d4af2bf40be9b31198288b479fd4bfec4f4c2
maxwell92 <710028463@qq.com>
2016-08-12 18:07:07 +0800

    add different API and url path test

--- iris-test/4wayToServeByIris.go
+++ iris-test/4wayToServeByIris.go
@@ -22,11 +22,26 @@ type UserAPI struct {
 }
 
 func (u UserAPI) Get() {
-	u.Write("Get from /Users")
+	u.Write("Get from /users")
 }
 
 func (u UserAPI) GetBy(id string) {
-	u.Write("Get from /Users/%s", id)
+	u.Write("Get from /users/%s", id)
+}
+
+type TempController struct {
+	*iris.Context
+}
+
+func (t TempController) Get() {
+	//id := u.Param("id")
+	t.Write("Get from /temp")
+	fmt.Println("this is Get()")
+}
+
+func (t TempController) GetBy(id string) {
+	t.Write("Get from /temp/%s", id)
+	fmt.Println("this is GetBy")
 }
 
 // Way 4: iris.Handle(Method, Route, iris.Handler)
@@ -42,6 +57,8 @@ func main() {
 	iris.Get("/", Index)
 	iris.HandleFunc("GET", "/handle", Handle)
 	iris.API("/users", UserAPI{})
+	iris.API("/temp", TempController{})
+	iris.API("/tmp", TempController{})
 	iris.Handle("GET", "/magic", &myHandler{"hello mush"})
 	iris.Listen(":8080")
 }
