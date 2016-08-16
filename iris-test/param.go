62f147c564662358f5a9363690776df8aadddb60
maxwell92 <710028463@qq.com>
2016-08-16 14:10:34 +0800

    getby access od

--- iris-test/param.go
+++ iris-test/param.go
@@ -22,6 +22,7 @@ func (ac AbcController) GetBy(id string) {
 	ac.Write("Get from /abc/" + id)
 }
 */
+/*
 func (ac AbcController) Get() {
 	od := ac.Param("od")
 	ld := ac.Param("ld")
@@ -31,23 +32,60 @@ func (ac AbcController) Get() {
 	//ac.Write("get from /abc")
 }
 
+func (ac AbcController) List() {
+	od := ac.Param("od")
+	fmt.Printf("get %s list\n", od)
+}
+*/
 /*
 func (ac AbcController) GetBy() {
 	od := ac.Param("od")
 	fmt.Printf("get from /abc/%s\n", od)
 }
 */
+/*
 func (ac AbcController) Post() {
 	od := ac.Param("od")
 	//ld := ac.Param("ld")
 	fmt.Printf("post from /abc/%s/lalala\n", od)
 
 }
+*/
+/*
+func (ac AbcController) Get(od string) {
+	fmt.Printf("Get from /abc/%s/deploy\n", od)
+}
+*/
+// iris.API("/abc", *abc) cann't run this
+/*
+func (ac AbcController) GetBy(od string, id string) {
+	fmt.Printf("Get from /abc/%s/deploy/%s\n", od, id)
+}
+*/
+
+//func (ac AbcController) Get(od string, id string) {
+func (ac AbcController) GetBy(od string, id string) {
+	//fmt.Printf("GetBy from /abc/%s/deploy/%s\n", od, id)
+	/*
+		if id == "" {
+			fmt.Printf("Get from /abc/%s/deploy\n", od)
+		} else {
+			fmt.Printf("GetBy from /abc/%s/deploy/%s\n", od, id)
+		}
+	*/
+	fmt.Printf("od: %s, id: %s\n", od, id)
+}
+
+func (ac AbcController) Post() {
+	fmt.Printf("Post from /abc/deploy\n")
+}
+
 func main() {
 	abc := new(AbcController)
 	//iris.API("/abc", *abc)
-	iris.API("/abc/:od/lalala/:ld", *abc)
-	iris.API("/abc/:od/lalala", *abc)
+	//iris.API("/abc/:od/lalala/:ld", *abc)
+	//iris.API("/abc/:od/lalala", *abc)
+	//iris.API("/abc", *abc)
 	iris.API("/abc", *abc)
 	iris.Listen(":10000")
 }
